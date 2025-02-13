054fd8ba44e291c6252db34d82f6f10a9f177551
leejy811 <lee38956162@gmail.com>
2025-02-14 04:19:39 +0900

    Add Off Well Sprite

--- Assets/Scripts/Object/Well.cs
+++ Assets/Scripts/Object/Well.cs
@@ -8,9 +8,13 @@ public class Well : InteractableObject
 {
     public BuffType buffType;
     public int[] buffValues;
+    public Sprite offSprite;
 
     protected override void OnInteraction(GameObject gameObject)
     {
         PlayerController.instance.GetBuff(buffType, buffValues[(int)buffType]);
+
+        interactable = false;
+        gameObject.GetComponent<SpriteRenderer>().sprite = offSprite;
     }
 }
